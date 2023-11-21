f801dd280bb7b094da610a4e526abb7329226b40
GuilhermeV <guiwojtysiak053@hotmail.com>
2023-11-21 16:15:22 -0300

    Initial commit

---
+++ src/Dados/Eventos/Seca.java
@@ -0,0 +1,20 @@
+package Dados.Eventos;
+
+public class Seca extends Evento{
+    private int estiagem;
+
+    public Seca(String codigo, String data, double latitude, double longitude, int estiagem) {
+        super(codigo, data, latitude, longitude);
+        this.estiagem = estiagem;
+    }
+
+    public int getEstiagem() {
+        return estiagem;
+    }
+
+    @Override
+    public String toString() {
+        return "=========================== \n" + "Seca: " + super.toString() +
+        "\n Estiagem: " + estiagem;
+    }
+}
